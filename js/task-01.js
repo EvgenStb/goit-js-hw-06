7e3d61c3c532ac9d66f283599711e571e22fb750
Yevhenii Strelbitskyi <strelbitskii.evgenii@gmail.com>
2023-01-03 10:22:22 +0200

    11

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,11 @@
-const sumElCategories = document.querySelectorAll("li.item");
-console.log("Numbers of category",sumElCategories.length);
-for (const element of sumElCategories) {
-    console.log("Category:",element.querySelector("h2").textContent);
-    console.log("Elements",element.querySelectorAll("li").length);
-}
+const body = document.body;
+const list = body.childNodes;
+console.log(list);
+
+
+// const sumElCategories = document.querySelectorAll("li.item");
+// console.log("Numbers of category",sumElCategories.length);
+// for (const element of sumElCategories) {
+//     console.log("Category:",element.querySelector("h2").textContent);
+//     console.log("Elements",element.querySelectorAll("li").length);
+// }
--- js/task-10.js
+++ js/task-10.js
@@ -1,16 +1,30 @@
-const collectionValue = document.getElementsByTagName('input');
+const collectionValue = document.querySelector('#controls input');
 const createCollectionBtn = document.querySelector('button[data-create]')
 const destroyCollectionBtn = document.querySelector('button[data-destroy]')
 const collectionContainer = document.getElementById('boxes')
-console.log(collectionContainer)
+const boxQuantity = collectionValue.value;
+console.log(collectionValue.value);
 
-
-
-
-
-
-console.log(destroyCollectionBtn)
-
+createCollectionBtn.addEventListener('click',handleBoxCreating)
+function handleBoxCreating (boxQuantity){
+  const boxCollection = [];
+  const width = 20;
+  const hight = 20;
+  for (let i=1; i <= boxQuantity; i +=1){
+    const box = document.createElement('div');
+    console.log (box);
+    box.width = `${width+10}px`
+    box.hight = `${hight+10}px`
+    box.backgroundColor = getRandomHexColor();
+    console.log(box);
+    // collectionContainer.append(...box);
+}}
+console.log(handleBoxCreating());
+console.log(boxCollection);
+console.log(collectionValue.currentTarget.value)
+// function createBoxes(amount) {
+//   for (let i = 1; )
+// }
 
 
 function getRandomHexColor() {
